cede9ea8fa6dcb9d7c268db41ee76d9b42dc09d2
Michael Santos <michael.santos@gmail.com>
2021-09-30 08:37:21 -0400

    x509: re-format

--- src/runlet_x509.erl
+++ src/runlet_x509.erl
@@ -3,34 +3,36 @@
 -include_lib("public_key/include/public_key.hrl").
 
 -export([
-         info/1,
+    info/1,
 
-         public_key/1,
-         version/1,
-         serial_number/1,
-         signature_algorithm/1,
-         issuer/1,
-         not_before/1,
-         not_after/1,
-         subject/1,
-         get_value/1
-        ]).
+    public_key/1,
+    version/1,
+    serial_number/1,
+    signature_algorithm/1,
+    issuer/1,
+    not_before/1,
+    not_after/1,
+    subject/1,
+    get_value/1
+]).
 
 info(X509) ->
-    #{data => #{
-        version => version(X509),
-        serialNumber => serial_number(X509)
-       },
+    #{
+        data => #{
+            version => version(X509),
+            serialNumber => serial_number(X509)
+        },
 
-      signatureAlgorithm => signature_algorithm(X509),
+        signatureAlgorithm => signature_algorithm(X509),
 
-      issuer => issuer(X509),
+        issuer => issuer(X509),
 
-      validity => #{
-        notBefore => not_before(X509),
-        notAfter => not_after(X509)
-       },
-      subject => subject(X509)}.
+        validity => #{
+            notBefore => not_before(X509),
+            notAfter => not_after(X509)
+        },
+        subject => subject(X509)
+    }.
 
 public_key(X509) ->
     X509#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.subjectPublicKeyInfo#'OTPSubjectPublicKeyInfo'.subjectPublicKey.
@@ -42,7 +44,9 @@ serial_number(X509) ->
     X509#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.serialNumber.
 
 signature_algorithm(X509) ->
-    sigalg(X509#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.signature#'SignatureAlgorithm'.algorithm).
+    sigalg(
+        X509#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.signature#'SignatureAlgorithm'.algorithm
+    ).
 
 issuer(X509) ->
     to_string(rdn_sequence(X509#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.issuer)).
@@ -57,22 +61,32 @@ subject(X509) ->
     to_string(rdn_sequence(X509#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.subject)).
 
 rdn_sequence({rdnSequence, Subject}) ->
-    [ {value(Attribute#'AttributeTypeAndValue'.type), 
-       case Attribute#'AttributeTypeAndValue'.value of
-           {_, Str} -> Str;
-           Str -> Str
-       end} || [Attribute] <- Subject ].
+    [
+        {
+            value(Attribute#'AttributeTypeAndValue'.type),
+            case Attribute#'AttributeTypeAndValue'.value of
+                {_, Str} -> Str;
+                Str -> Str
+            end
+        }
+     || [Attribute] <- Subject
+    ].
 
 get_value(OTPCert) ->
     {rdnSequence, Subject} = OTPCert#'OTPCertificate'.tbsCertificate#'OTPTBSCertificate'.subject,
-    [ {value(Attribute#'AttributeTypeAndValue'.type),
-       case Attribute#'AttributeTypeAndValue'.value of
-           {_, Str} -> Str;
-           Str -> Str
-       end} || [Attribute] <- Subject ].
+    [
+        {
+            value(Attribute#'AttributeTypeAndValue'.type),
+            case Attribute#'AttributeTypeAndValue'.value of
+                {_, Str} -> Str;
+                Str -> Str
+            end
+        }
+     || [Attribute] <- Subject
+    ].
 
 to_string(N) ->
-    lists:concat(lists:concat([ [X, ": ", maybe_string(Y), "\n"] || {X,Y} <- N ])).
+    lists:concat(lists:concat([[X, ": ", maybe_string(Y), "\n"] || {X, Y} <- N])).
 
 maybe_string(N) when is_binary(N) -> binary_to_list(N);
 maybe_string(N) -> N.
@@ -80,7 +94,7 @@ maybe_string(N) -> N.
 utctime({utcTime, UTC}) ->
     utctime(list_to_binary(UTC));
 utctime(<<YY:2/bytes, MM:2/bytes, DD:2/bytes, HH:2/bytes, NN:2/bytes, SS:2/bytes, "Z">>) ->
-    binary_to_list(list_to_binary(["20", YY, "-", MM , "-", DD, "T", HH, ":", NN, ":", SS, "Z"])).
+    binary_to_list(list_to_binary(["20", YY, "-", MM, "-", DD, "T", HH, ":", NN, ":", SS, "Z"])).
 
 value(?'dhKeyAgreement') -> "dhKeyAgreement";
 value(?'pkcs-3') -> "pkcs-3";
@@ -321,19 +335,28 @@ value(?'id-kp') -> "id-kp";
 value(?'id-qt') -> "id-qt";
 value(?'id-pe') -> "id-pe";
 value(?'id-pkix') -> "id-pkix";
-
 value(_) -> "unknown".
 
-sigalg(?'id-dsa-with-sha1') -> "dsa-with-sha1";
-sigalg(?'id-dsaWithSHA1') -> "dsaWithSHA1";
-sigalg(?'md2WithRSAEncryption') -> "md2WithRSAEncryption";
-sigalg(?'md5WithRSAEncryption') -> "md5WithRSAEncryption";
-sigalg(?'sha1WithRSAEncryption') -> "sha1WithRSAEncryption";
-sigalg(?'sha-1WithRSAEncryption') -> "sha-1WithRSAEncryption";
-sigalg(?'sha224WithRSAEncryption') -> "sha224WithRSAEncryption";
-sigalg(?'sha256WithRSAEncryption') -> "sha256WithRSAEncryption";
-sigalg(?'sha512WithRSAEncryption') -> "sha512WithRSAEncryption";
-sigalg(?'ecdsa-with-SHA1') -> "ecdsa-with-SHA1";
+sigalg(?'id-dsa-with-sha1') ->
+    "dsa-with-sha1";
+sigalg(?'id-dsaWithSHA1') ->
+    "dsaWithSHA1";
+sigalg(?'md2WithRSAEncryption') ->
+    "md2WithRSAEncryption";
+sigalg(?'md5WithRSAEncryption') ->
+    "md5WithRSAEncryption";
+sigalg(?'sha1WithRSAEncryption') ->
+    "sha1WithRSAEncryption";
+sigalg(?'sha-1WithRSAEncryption') ->
+    "sha-1WithRSAEncryption";
+sigalg(?'sha224WithRSAEncryption') ->
+    "sha224WithRSAEncryption";
+sigalg(?'sha256WithRSAEncryption') ->
+    "sha256WithRSAEncryption";
+sigalg(?'sha512WithRSAEncryption') ->
+    "sha512WithRSAEncryption";
+sigalg(?'ecdsa-with-SHA1') ->
+    "ecdsa-with-SHA1";
 sigalg(Alg) ->
     {DigestType, SignatureType} = public_key:pkix_sign_types(Alg),
     lists:concat([DigestType, "/", SignatureType]).
