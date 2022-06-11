1a5c1ce4a0430d532d57ca93c369fe6813b25223
Michael Santos <michael.santos@gmail.com>
2022-06-11 08:38:28 -0400

    1.0.2
    * update deps
    * fix tests

--- test/runlet_cmd_http_test.exs
+++ test/runlet_cmd_http_test.exs
@@ -1,18 +1,20 @@
 defmodule RunletCmdHTTPTest do
   use ExUnit.Case
 
-  result = Runlet.Cmd.HTTP.exec('http://httpbin.org/status/200')
+  test "http: request URL" do
+    result = Runlet.Cmd.HTTP.exec("http://httpbin.org/status/200")
 
-  assert [
-           %Runlet.Event{
-             attr: %{},
-             event: %Runlet.Event.Stdout{
-               description: [],
-               host: "nonode@nohost",
-               service: "http",
-               time: ""
-             },
-             query: "http http://httpbin.org/status/200"
-           }
-         ] = result
+    assert [
+             %Runlet.Event{
+               attr: %{},
+               event: %Runlet.Event.Stdout{
+                 description: [],
+                 host: "nonode@nohost",
+                 service: "http",
+                 time: ""
+               },
+               query: "http http://httpbin.org/status/200"
+             }
+           ] = result
+  end
 end
--- test/runlet_cmd_tls_test.exs
+++ test/runlet_cmd_tls_test.exs
@@ -1,111 +1,113 @@
 defmodule RunletCmdTLSTest do
   use ExUnit.Case
 
-  url = Runlet.Cmd.TLS.exec("httpbin.org")
-  urlport = Runlet.Cmd.TLS.exec("httpbin.org:443")
-  arity2 = Runlet.Cmd.TLS.exec("httpbin.org", 443)
+  test "tls: get certificate details" do
+    url = Runlet.Cmd.TLS.exec("httpbin.org")
+    urlport = Runlet.Cmd.TLS.exec("httpbin.org:443")
+    arity2 = Runlet.Cmd.TLS.exec("httpbin.org", 443)
 
-  # ensure bad certificates can be dumped
-  untrusted_root = Runlet.Cmd.TLS.exec("untrusted-root.badssl.com")
-  expired = Runlet.Cmd.TLS.exec("expired.badssl.com")
-  sha1 = Runlet.Cmd.TLS.exec("sha1-intermediate.badssl.com")
-  des3 = Runlet.Cmd.TLS.exec("3des.badssl.com")
+    # ensure bad certificates can be dumped
+    untrusted_root = Runlet.Cmd.TLS.exec("untrusted-root.badssl.com")
+    expired = Runlet.Cmd.TLS.exec("expired.badssl.com")
+    sha1 = Runlet.Cmd.TLS.exec("sha1-intermediate.badssl.com")
+    des3 = Runlet.Cmd.TLS.exec("3des.badssl.com")
 
-  assert [
-           %Runlet.Event{
-             attr: %{},
-             event: %Runlet.Event.Stdout{
-               description: _,
-               host: "httpbin.org:443",
-               service: "tls",
-               time: ""
-             },
-             query: "tls httpbin.org:443"
-           }
-           | _
-         ] = url
+    assert [
+             %Runlet.Event{
+               attr: %{},
+               event: %Runlet.Event.Stdout{
+                 description: _,
+                 host: "httpbin.org:443",
+                 service: "tls",
+                 time: ""
+               },
+               query: "tls httpbin.org:443"
+             }
+             | _
+           ] = url
 
-  assert [
-           %Runlet.Event{
-             attr: %{},
-             event: %Runlet.Event.Stdout{
-               description: _,
-               host: "httpbin.org:443",
-               service: "tls",
-               time: ""
-             },
-             query: "tls httpbin.org:443"
-           }
-           | _
-         ] = urlport
+    assert [
+             %Runlet.Event{
+               attr: %{},
+               event: %Runlet.Event.Stdout{
+                 description: _,
+                 host: "httpbin.org:443",
+                 service: "tls",
+                 time: ""
+               },
+               query: "tls httpbin.org:443"
+             }
+             | _
+           ] = urlport
 
-  assert [
-           %Runlet.Event{
-             attr: %{},
-             event: %Runlet.Event.Stdout{
-               description: _,
-               host: "httpbin.org:443",
-               service: "tls",
-               time: ""
-             },
-             query: "tls httpbin.org:443"
-           }
-           | _
-         ] = arity2
+    assert [
+             %Runlet.Event{
+               attr: %{},
+               event: %Runlet.Event.Stdout{
+                 description: _,
+                 host: "httpbin.org:443",
+                 service: "tls",
+                 time: ""
+               },
+               query: "tls httpbin.org:443"
+             }
+             | _
+           ] = arity2
 
-  assert [
-           %Runlet.Event{
-             attr: %{},
-             event: %Runlet.Event.Stdout{
-               description: <<"[protocol: :\"tlsv", _::binary>>,
-               host: "untrusted-root.badssl.com:443",
-               service: "tls",
-               time: ""
-             },
-             query: "tls untrusted-root.badssl.com:443"
-           }
-           | _
-         ] = untrusted_root
+    assert [
+             %Runlet.Event{
+               attr: %{},
+               event: %Runlet.Event.Stdout{
+                 description: <<"[protocol: :\"tlsv", _::binary>>,
+                 host: "untrusted-root.badssl.com:443",
+                 service: "tls",
+                 time: ""
+               },
+               query: "tls untrusted-root.badssl.com:443"
+             }
+             | _
+           ] = untrusted_root
 
-  assert [
-           %Runlet.Event{
-             attr: %{},
-             event: %Runlet.Event.Stdout{
-               description: <<"[protocol: :\"tlsv", _::binary>>,
-               host: "expired.badssl.com:443",
-               service: "tls",
-               time: ""
-             },
-             query: "tls expired.badssl.com:443"
-           }
-           | _
-         ] = expired
+    assert [
+             %Runlet.Event{
+               attr: %{},
+               event: %Runlet.Event.Stdout{
+                 description: <<"[protocol: :\"tlsv", _::binary>>,
+                 host: "expired.badssl.com:443",
+                 service: "tls",
+                 time: ""
+               },
+               query: "tls expired.badssl.com:443"
+             }
+             | _
+           ] = expired
 
-  assert [
-           %Runlet.Event{
-             attr: %{},
-             event: %Runlet.Event.Stdout{
-               description: <<"[protocol: :\"tlsv", _::binary>>,
-               host: "sha1-intermediate.badssl.com:443",
-               service: "tls",
-               time: ""
-             },
-             query: "tls sha1-intermediate.badssl.com:443"
-           }
-           | _
-         ] = sha1
+    assert [
+             %Runlet.Event{
+               attr: %{},
+               event: %Runlet.Event.Stdout{
+                 description: <<"[protocol: :\"tlsv", _::binary>>,
+                 host: "sha1-intermediate.badssl.com:443",
+                 service: "tls",
+                 time: ""
+               },
+               query: "tls sha1-intermediate.badssl.com:443"
+             }
+             | _
+           ] = sha1
 
-  assert [
-           %Runlet.Event{
-             attr: %{},
-             event: %Runlet.Event.Stdout{
-               description: <<"[protocol: :\"tlsv", _::binary>>,
-               host: "3des.badssl.com:443",
-               service: "tls",
-               time: ""
-             },
-             query: "tls 3des.badssl.com:443"
-           }
-           | _
-         ] = des3
+    assert [
+             %Runlet.Event{
+               attr: %{},
+               event: %Runlet.Event.Stdout{
+                 description: <<"[protocol: :\"tlsv", _::binary>>,
+                 host: "3des.badssl.com:443",
+                 service: "tls",
+                 time: ""
+               },
+               query: "tls 3des.badssl.com:443"
+             }
+             | _
+           ] = des3
+  end
 end
